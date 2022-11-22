d0fb73a8b66745b57ab8e85a9a6afd78ab95d424
MarsKO <marcosilhaag31@gmail.com>
2022-11-21 21:49:17 -0300

    Transições suaves; Correção tela branca; testes

--- main.js
+++ main.js
@@ -1,23 +1,51 @@
 import {theme} from './js/theme.js'
 
 const background = document.getElementById('background')
-let arrBackground = theme('day')
+let previousBackgroundTheme = 'day'
+let currentBackgroundTheme = 'day'
+let arrBackground = theme(currentBackgroundTheme)
 let arrBackgroundIndex = 1
 
 background.classList.toggle('slide')
 
+// ROTINA PARA TROCA DE TEMA DINÂMICA
 background.addEventListener('animationend', ()=>{
-    if(arrBackgroundIndex == (arrBackground.length-1)){
-        document.body.style.setProperty('--bgstart', arrBackground[arrBackgroundIndex])
-        document.body.style.setProperty('--bgend', arrBackground[0])
-        arrBackgroundIndex=0
+    if(currentBackgroundTheme === previousBackgroundTheme){
+        if(arrBackgroundIndex == (arrBackground.length-1)){
+            document.body.style.setProperty('--backgroundStart', arrBackground[arrBackgroundIndex])
+            document.body.style.setProperty('--backgroundEnd', arrBackground[0])
+            arrBackgroundIndex=0
+        }else{
+            document.body.style.setProperty('--backgroundStart', arrBackground[arrBackgroundIndex])
+            document.body.style.setProperty('--backgroundEnd', arrBackground[arrBackgroundIndex+1])
+            arrBackgroundIndex++
+        }
+        background.classList.toggle('slide')
+        void background.offsetWidth
+        background.classList.toggle('slide')
     }else{
-        document.body.style.setProperty('--bgstart', arrBackground[arrBackgroundIndex])
-        document.body.style.setProperty('--bgend', arrBackground[arrBackgroundIndex+1])
-        arrBackgroundIndex++
+        previousBackgroundTheme = currentBackgroundTheme
+
+        document.body.style.setProperty('--backgroundStart', arrBackground[arrBackgroundIndex])
+        document.body.style.setProperty('--backgroundEnd', theme(currentBackgroundTheme)[0])
+
+        arrBackgroundIndex=0
+        arrBackground = theme(currentBackgroundTheme)
+
+        background.classList.toggle('slide')
+        void background.offsetWidth
+        background.classList.toggle('slide')
     }
-    background.classList.toggle('slide')
-    void background.offsetWidth
-    background.classList.toggle('slide')
 })
 
+btnNight.addEventListener('click', ()=>{
+    currentBackgroundTheme = 'night'
+})
+
+btnRain.addEventListener('click', ()=>{
+    currentBackgroundTheme = 'rain'
+})
+
+btnDay.addEventListener('click',()=>{
+    currentBackgroundTheme = 'day'
+})
