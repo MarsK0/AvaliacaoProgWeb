7196bbef0a524189e28ea956bff8580c8a0fdbd5
MarsK0 <marcosilhaag31@gmail.com>
2022-11-23 12:52:15 -0300

    Add player música

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
 import {runTheme} from './js/theme.js'
 
+const themeSong = document.getElementById('themeSong')
+const themeSongVolume = document.getElementById('themeSongVolume')
 const background = document.getElementById('background')
 //Evento de troca dinâmica dos slides, rotina no arquivo theme.js
 background.addEventListener('animationend', ()=>{
     runTheme()
 })
+
+btnThemeSongPlay.addEventListener('click', ()=>{
+    if(themeSong.paused){
+        themeSong.play()
+        btnThemeSongPlay.innerText = '| |'
+    }else{
+        themeSong.pause()
+        btnThemeSongPlay.innerText = '|>'
+    }
+})
+
+themeSongVolume.oninput = function(){
+    themeSong.volume = (document.getElementById('themeSongVolume').value)/100
+}
+
+
+
