4c9e88e9f52756d34fff1a18b68e8b8257cd2b97
MarsKO <marcosilhaag31@gmail.com>
2022-11-28 21:37:35 -0300

    Finalizado 1.0

--- js/home.js
+++ js/home.js
@@ -1,4 +1,10 @@
 let msgGlobalId = -1
+const tbody = document.getElementsByTagName('tbody')[0]
+console.log(tbody.textContent)
+
+if(tbody.textContent === ''){
+    geraTabela()
+}
 
 editMsgSave.addEventListener('click', ()=>{
     let editMsgDesc = document.getElementById('editMsgDesc')
@@ -9,18 +15,24 @@ editMsgSave.addEventListener('click', ()=>{
             return
         }
 
+        const users = JSON.parse(localStorage.getItem('users'))
         const userLogin = JSON.parse(localStorage.getItem('userLogin'))
         const userLoginArrMsg = userLogin.arrMsg
+
         let arrMsg = [editMsgDesc.value,editMsgDetail.value]
+
         userLoginArrMsg.push(arrMsg)
+        users[userLogin.id].arrMsg = userLoginArrMsg
+        
         localStorage.setItem('userLogin',JSON.stringify(userLogin))
+        localStorage.setItem('users', JSON.stringify(users))
 
         editMsgDesc.value = ''
         editMsgDetail.value = ''
+        geraTabela()
     }else{
         if(editMsgDesc.value === '' || editMsgDetail.value === ''){
             alert('Preencha ambos os campos!')
-            
             return
         }
 
@@ -39,6 +51,7 @@ editMsgSave.addEventListener('click', ()=>{
         editMsgDesc.value = ''
         editMsgDetail.value = ''
         msgGlobalId = -1
+        geraTabela()
     }
 })
 
@@ -56,62 +69,76 @@ editMsgCancel.addEventListener('click',()=>{
 })
 
 function editMsg(tableMsgListEdit){
-    let editMsgDescVal = document.getElementById('editMsgDesc')
-    let editMsgDetailVal = document.getElementById('editMsgDetail')
+    let editMsgDesc = document.getElementById('editMsgDesc')
+    let editMsgDetail = document.getElementById('editMsgDetail')
     const msgRow = tableMsgListEdit.parentElement.parentElement
     const msgId = Number(msgRow.querySelector('td').innerHTML) - 1
 
     const userLogin = JSON.parse(localStorage.getItem('userLogin'))
     const userLoginArrMsg = userLogin.arrMsg
 
-    editMsgDescVal.value = userLoginArrMsg[msgId][0]
-    editMsgDetailVal.value = userLoginArrMsg[msgId][1]
+    editMsgDesc.value = userLoginArrMsg[msgId][0]
+    editMsgDetail.value = userLoginArrMsg[msgId][1]
 
     msgGlobalId = msgId
+    geraTabela()
 }
 
 function deleteMsg(tableMsgListDelete){
     const msgRow = tableMsgListDelete.parentElement.parentElement
-    const msgId = msgRow.querySelector('td').innerHTML
+    const msgId = Number(msgRow.querySelector('td').innerHTML) - 1
+
+    const users = JSON.parse(localStorage.getItem('users'))
+    const userLogin = JSON.parse(localStorage.getItem('userLogin'))
+    let userLoginArrMsg = userLogin.arrMsg
+
+    delete userLogin.arrMsg[msgId]
+    userLoginArrMsg = userLoginArrMsg.filter(e => e != null)
+    
+    users[userLogin.id].arrMsg = userLoginArrMsg
+    userLogin.arrMsg = userLoginArrMsg
+
+    localStorage.setItem('userLogin', JSON.stringify(userLogin))
+    localStorage.setItem('users', JSON.stringify(users))
+    geraTabela()
 }
 
 function geraTabela(){
-    const tbody = document.getElementsByTagName('tbody')[0]
     tbody.innerHTML = ''
 
     const userLogin = JSON.parse(localStorage.getItem('userLogin'))
     const userLoginArrMsg = userLogin.arrMsg
-
-    userLoginArrMsg.forEach((e, i)=>{
-        let row = document.createElement('tr')
-        let msgId = document.createElement('td')
-        let msgDesc = document.createElement('td')
-        let msgDetail = document.createElement('td')
-        let rowButtons = document.createElement('td')
-        let btnMsgListEdit = document.createElement('button')
-        let btnMsgListDelete = document.createElement('button')
+    if(userLoginArrMsg != null){
+        userLoginArrMsg.forEach((e, i)=>{
+            let row = document.createElement('tr')
+            let msgId = document.createElement('td')
+            let msgDesc = document.createElement('td')
+            let msgDetail = document.createElement('td')
+            let rowButtons = document.createElement('td')
+            let btnMsgListEdit = document.createElement('button')
+            let btnMsgListDelete = document.createElement('button')
+        
+            btnMsgListEdit.classList.add('tableMsgListEdit')
+            btnMsgListEdit.setAttribute('onclick','editMsg(this)')
+            btnMsgListEdit.innerHTML = 'EDITAR'
     
-        btnMsgListEdit.classList.add('tableMsgListEdit')
-        btnMsgListEdit.setAttribute('onclick','editMSg(this)')
-        btnMsgListEdit.innerHTML = 'EDITAR'
-
-        btnMsgListDelete.classList.add('tableMsgListDelete')
-        btnMsgListDelete.setAttribute('onclick','deleteMsg(this)')
-        btnMsgListDelete.innerHTML = 'EXCLUIR'
-
-        rowButtons.appendChild(btnMsgListEdit)
-        rowButtons.appendChild(btnMsgListDelete)
-
-        msgId.innerHTML = i+1
-        msgDesc.innerHTML = e[0]
-        msgDetail.innerHTML = e[1]
-
-        row.appendChild(msgId)
-        row.appendChild(msgDesc)
-        row.appendChild(msgDetail)
-        row.appendChild(rowButtons)
-
-        tbody.appendChild(row)
-    })
+            btnMsgListDelete.classList.add('tableMsgListDelete')
+            btnMsgListDelete.setAttribute('onclick','deleteMsg(this)')
+            btnMsgListDelete.innerHTML = 'EXCLUIR'
     
+            rowButtons.appendChild(btnMsgListEdit)
+            rowButtons.appendChild(btnMsgListDelete)
+    
+            msgId.innerHTML = i+1
+            msgDesc.innerHTML = e[0]
+            msgDetail.innerHTML = e[1]
+    
+            row.appendChild(msgId)
+            row.appendChild(msgDesc)
+            row.appendChild(msgDetail)
+            row.appendChild(rowButtons)
+    
+            tbody.appendChild(row)
+        })
+    }
 }
--- main.js
+++ main.js
@@ -12,11 +12,9 @@ export class User {
 function setUserAdmin(){
     let users = JSON.parse(localStorage.getItem('users'))
     let userAdmin = new User('admin','admin',[])
-    let userTeste = new User('teste','123',[])
     if(users === null){
         users = []
         users.push(userAdmin)
-        users.push(userTeste)
         localStorage.setItem('users',JSON.stringify(users))
     }
 }
